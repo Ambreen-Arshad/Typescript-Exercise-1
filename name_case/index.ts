99565998e9017b75358834e6b4da070c8ad6446b
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-25 14:24:11 +0500

    name_case

---
+++ name_case/index.ts
@@ -0,0 +1,14 @@
+// Date 25-4-2024
+
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, \n
+// uppercase, and titlecase.
+
+
+let personName : string = "ambreen"; // this save the name
+console.log(personName.toLowerCase()); // shows the name in all small letters
+
+console.log(personName.toUpperCase()); // shows the name in all Big letters
+
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()); // shows the personName with the first letter big.
+
